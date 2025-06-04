d6b001f25fffeb73cbc6f9aa62d8cadd43bb22fd
LemosFTW <rl.fernandes@campus.fct.unl.pt>
2025-06-04 20:57:34 -0300

    Bugfix: Fetching data Calendar events

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -2,8 +2,27 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import ptBrLocale from "@fullcalendar/core/locales/pt-br";
+import { useEffect, useState} from "react";
+import { get, post, del, patch } from "@/middleware/axios";
+import { Task } from "@/interfaces/types";
+
 
 export default function CalendarPage() {
+  const [appointments, setAppointments] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      await get('appointments').then(res => {
+        console.log('Response:', res.appointments);
+        setAppointments(res.appointments);
+      }).catch(err => {
+        console.log(err);
+      });
+    };
+    fetchAppointments();
+  }, []);
+    
+
   return (
     <div className="w-full min-h-screen bg-gray-900 p-4">
       <FullCalendar
@@ -11,9 +30,13 @@ export default function CalendarPage() {
         initialView="dayGridMonth"
         locale={ptBrLocale}
         height="auto"
-        events={[
-          { title: "Reunião de Equipe", date: "2025-05-28" },
-        ]}
+        eventColor="#1976d2" 
+        events={appointments.map(app => ({
+          id: app.id,
+          title: app.name,
+          start: app.date,
+          description: app.description
+        }))}
       />
     </div>
   );
